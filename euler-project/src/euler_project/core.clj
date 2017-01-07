91df197a7622086eeb160518676d1f186618414d
JaewoongMoon <mjw8585@gmail.com>
2017-01-08 02:05:17 +0900

    add euler ex20

--- euler-project/src/euler_project/core.clj
+++ euler-project/src/euler_project/core.clj
@@ -607,3 +607,32 @@ ex11-num
 (defn check-cnt [n]
 
 )
+
+
+; ex20) n! 이라는 표기법은 n × (n − 1) × ... × 3 × 2 × 1을 뜻합니다.
+
+;예를 들자면 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800 이 되는데,
+;여기서 10!의 각 자리수를 더해 보면 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27 입니다.
+
+;100! 의 자리수를 모두 더하면 얼마입니까?
+
+(defn factorial [n]
+  (loop [i n acc 1]
+     (if (= i 1)
+       acc
+       (recur (dec i)
+               (*' acc i)))))
+
+
+(defn sum-number-char [n]
+  (let [ num-list (clojure.string/split (str n) #"")]
+    
+    )
+
+
+
+(loop [idx 1]
+  (let [ num (tri-num idx)]
+    (if (>= (factor-cnt num) 500)
+      num
+      (recur (inc idx)))))
